c4023bb00b03e57e47d7f8cc56230bdbe2b9706c
Green <regnault.marien@gmail.com>
2020-05-21 11:33:35 +0200

    Symfony quest 5 - Creating routes with Symfony

--- src/Controller/ZapperController.php
+++ src/Controller/ZapperController.php
@@ -6,15 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/zapper", name="zapper_")
+ */
 class ZapperController extends AbstractController
 {
     /**
-     * @Route("/zapper", name="zapper_index")
+     * @Route("/zapper", name="index")
      */
     public function index() :Response
     {
         return $this->render('zapper/index.html.twig', [
             'website' => 'Zapper Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ0-9-]+"}, utf8=true, name="show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug = ''): Response
+    {
+        $seriesTitle = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('zapper/show.html.twig', [
+            'title' => $seriesTitle,
         ]);
     }
 }
